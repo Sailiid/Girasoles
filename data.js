afe294836fd2266baf2527c22ab0fb2209a73a91
sailiid <neymarsaime@gmail.com>
2024-03-06 13:23:59 -0500

    agregue header y carrusel

---
+++ data.js
@@ -0,0 +1,94 @@
+const data = {
+    
+    modelos: [
+      {
+        id: "1",
+        name: "Balaca Sirena",
+        tamaño:"7 x 5 cm",
+        image:"imagenes/sirena.jpg",
+        date: "",
+        description:
+          "Balaca doble cinta de 3.8 cm, cinta especial para cada balaca.",
+        category: "Sirena",
+        price: 8000 ,
+       },
+       {
+        id: "2",
+        name: "Boutique-Unicolor 0.1",
+        tamaño:"11 x 6 cm",
+        image: "imagenes/boutique-3.1.jpg",
+        date: "",
+        description:
+          "Moño Unicolor grande con cola",
+        category: "Boutique",
+        price: 5000 ,
+       },
+       {
+        id: "3",
+        name: "Boutique-Unicolor 0.2",
+        tamaño:"12 x 6 cm",
+        image: "imagenes/b-lila.jpg",
+        date: "",
+        description:
+          "Moño boutique unicolor grande.",
+        category: "Boutique",
+        price: 8000 ,
+       },
+       {
+        id: "4",
+        name: "Boutique-pequeño",
+        tamaño:"7 x 5 cm",
+        image: "imagenes/boutique-pequeño.jpg",
+        date: "",
+        description:
+          "Promo de boutique pequeño por docenas. ",
+        category: "Boutique",
+        price:"",
+       },
+       {
+        id: "5",
+        name: "Boutique 0.3",
+        tamaño:"12 x 6 cm",
+        image: "imagenes/boutique2.jpg",
+        date: "",
+        description:
+          "Moño unicolor boutique grande",
+        category: "",
+        price: 8000,
+       },
+       {
+        id: "6",
+        name: "Duo Color",
+        tamaño:"12 x 6 cm",
+        image: "imagenes/entrelazados.jpg",
+        date: "",
+        description:
+          "Moño grande, duo color. ",
+        category: "Duo",
+        price: 4000 ,
+       },
+       {
+        id: "7",
+        name: "Lazo Amor",
+        tamaño:"",
+        image: "imagenes/balaca-amor.jpg",
+        date: "",
+        description:
+          "balacas grandes de tela liberpull  ",
+        category: "Balaca",
+        price:14000 ,
+       },
+       {
+        id: "8",
+        name: "Especial",
+        tamaño:"",
+        image: "imagenes/captus.jpg",
+        date: "",
+        description:
+          "Moños especiales, con cintas del motivo que desees y apliques a tu gusto",
+        category: "Especial",
+        price: 10000 ,
+       },
+
+    ]
+}
